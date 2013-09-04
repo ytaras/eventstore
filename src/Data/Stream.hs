17225a2face3881dbbb45a29ba2f595b22d63241
ytaras <yura.taras@gmail.com>
2013-09-04 11:37:45 +0000

    API for fmap

--- src/Data/Stream.hs
+++ src/Data/Stream.hs
@@ -7,6 +7,7 @@ module Data.Stream ( createInputStream
                    , fakeManager
                    , StreamTransaction
                    , StreamPersistable
+                   , Stream
                    )
 where       
 
@@ -15,7 +16,7 @@ import Control.Monad.Error
 
 data Stream a = InputStream [a] |  MappedStream (StreamFunction a)
 newtype StreamTransaction a = StreamTransaction a
-type StreamFunction a = (MonadError StreamError m) => a -> m Value
+type StreamFunction a = (MonadError StreamError m) => Value -> m b
 data StreamManager = StreamManager
 type StreamError = String
 
@@ -24,13 +25,13 @@ class (FromJSON a, ToJSON a) => StreamPersistable a
 createInputStream :: StreamPersistable a => a -> Stream a
 createInputStream init = InputStream [init]
 
-persistStream :: StreamPersistable a => Stream a -> String -> StreamTransaction ()
+persistStream :: StreamPersistable a => String -> Stream a -> StreamTransaction (Stream a)
 persistStream = undefined
 
 executeTransaction :: StreamManager -> StreamTransaction a -> IO a
 executeTransaction = undefined
 
-liftStream :: (MonadError StreamError m, StreamPersistable a, StreamPersistable b) => (a -> m b) -> StreamFunction a
+liftStream :: (MonadError StreamError m, StreamPersistable a, StreamPersistable b) => (a -> m b) -> StreamFunction b
 liftStream = undefined
 
 fakeManager :: StreamManager
@@ -39,3 +40,6 @@ fakeManager = undefined
 instance Monad StreamTransaction where
     return = undefined
     (>>=) = undefined
+    
+instance Functor Stream where
+    fmap = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE TemplateHaskell, OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell, 
+             OverloadedStrings, 
+             TypeSynonymInstances,     
+             FlexibleInstances #-}
+             
 -- | Main entry point to the application.
 
 module Main where
@@ -7,32 +11,28 @@ import Data.Stream
 import Data.Aeson.TH
 import Data.Aeson
 import Data.Typeable
-import Data.Data
+import Data.Data  
 
-action :: StreamTransaction Bool
-action = do
-    let stream = createInputStream CounterReset
-    persistStream stream "Input"
-    return True
-
-badaboom :: IO Bool
-badaboom = executeTransaction fakeManager action 
-
+data CounterEvent = CounterAdded Int | CounterReset deriving (Show, Eq)
+newtype CounterState = CounterState Int deriving Show
+$(deriveJSON id ''CounterEvent)
+ 
 -- | The main entry point.
 main :: IO ()
 main = do 
    print (decode $ encode $ CounterAdded 4 :: Maybe CounterEvent)
-   t <- badaboom
+   t <- executeTransaction fakeManager action 
    print t
 
-data CounterEvent = CounterAdded Int | CounterReset deriving (Show, Eq)
-newtype CounterState = CounterState Int deriving Show
+action :: StreamTransaction Bool
+action = do
+    stream <- persistStream "Input" $ createInputStream CounterReset
+    persistStream "Output" $ fmap show stream
+    return True 
 
 counter :: CounterState -> CounterEvent -> CounterState
 counter _ CounterReset = CounterState 0
 counter (CounterState b) (CounterAdded a) = CounterState $ a + b
 
-$(deriveJSON id ''CounterEvent)
 instance StreamPersistable CounterEvent
-
-
+instance StreamPersistable String
